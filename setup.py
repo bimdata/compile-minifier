c3d248d733701c8479afee955db3e9d9949b0320
Antoine Mineau <antoine@bimdata.io>
2020-04-09 08:19:28 +0200

    fix circle requirements

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ from io import open
 
 
 NAME = "compile-minifier"
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    REQUIRES = f.read().strip().split('\n')
+with open("requirements/base.txt") as f:
+    REQUIRES = f.read().strip().split("\n")
+
+with open("requirements/ci.txt") as f:
+    CI_REQUIRES = f.read().strip().split("\n")
+
 
 setup(
     name=NAME,
@@ -22,20 +26,17 @@ setup(
     author_email="contact@bimdata.io",
     url="https://github.com/bimdata/compile-minifier",
     install_requires=REQUIRES,
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    python_requires='>=3.5, <4',
+    extras_require={"ci": CI_REQUIRES,},
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.5, <4",
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'compileminify=main:entrypoint',
-        ],
-    },
+    entry_points={"console_scripts": ["compileminify=main:entrypoint",],},
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
