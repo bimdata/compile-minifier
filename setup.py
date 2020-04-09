50dec9cf6893490d5204574b9149ce1b55f08c0f
Antoine Mineau <antoine@bimdata.io>
2020-04-09 08:03:34 +0200

    add circleci script

--- setup.py
+++ setup.py
@@ -2,23 +2,16 @@ from setuptools import setup, find_packages
 from os import path
 from io import open
 
+
+NAME = "compile-minifier"
+VERSION = '0.1.0'
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-NAME = "compile-minifier"
-VERSION = '0.1.0'
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
-
-REQUIRES = [
-    "python-minifier",
-    "fire"
-]
+with open('requirements.txt') as f:
+    REQUIRES = f.read().strip().split('\n')
 
 setup(
     name=NAME,
@@ -31,7 +24,7 @@ setup(
     install_requires=REQUIRES,
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    python_requires='>=2.7, !=3.0, !=3.1, !=3.2, <4',
+    python_requires='>=3.5, <4',
     include_package_data=True,
     entry_points={
         'console_scripts': [
@@ -39,11 +32,7 @@ setup(
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
--- src/main.py
+++ src/main.py
@@ -8,20 +8,28 @@ import python_minifier
 class CompileMinify(object):
     """Minify, compile and remove python files"""
 
-    def __init__(self, directory=os.getcwd()):
-        print(directory)
+    def __init__(self, directory: str = os.getcwd()):
+        if not os.path.exists(directory):
+            raise Exception('The directory "{}" was not found'.format(directory))
         self.directory = directory
 
-    def run(self, remove_tests: bool = False, remove_py: bool = False):
+    def run(
+        self,
+        remove_tests: bool = False,
+        remove_py: bool = False,
+        compile_root_py: bool = False,
+    ):
         self._apply_to_python_files(self._minify_and_compile, remove_tests, remove_py)
         print("Python files minified and compiled")
 
     def _apply_to_python_files(self, function, *args, **kwargs):
+        # No compile python files in root folder
+        # to preserve entrypoint
         level = 0
         for r, d, f in os.walk(self.directory):
             if r.endswith("tests") and kwargs["remove_tests"]:
                 shutil.rmtree(r)
-            elif level > 0:
+            elif level > 0 or kwargs["compile_root_py"]:
                 for file in f:
                     if file.endswith(".py"):
                         file = os.path.join(r, file)
